a3968672d6d6300aa92f356c0e62d7086fd0bccd
Tony Bowden <tony@tmtm.com>
2015-09-30 07:55:24 +0100

    only fetch 2013 members

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,7 @@
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
-WikiData::Category.new('تصنيف:أعضاء_مجلس_الأمة_الكويتي', 'ar').wikidata_ids.each do |id|
+WikiData::Category.new('تصنيف:أعضاء_مجلس_الأمة_الكويتي_2013', 'ar').wikidata_ids.each do |id|
   data = WikiData::Fetcher.new(id: id).data('ar') or next
   ScraperWiki.save_sqlite([:id], data)
 end
